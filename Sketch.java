cd8969f7de59f14814d9be10ed9c885303b9ffc5
Edwin_liu1 <Edwin.liu25@ycdsbk12.ca>
2024-04-18 14:11:54 -0400

    made a bouncing ball with a class

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,20 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
+  // need to get images...
+
+  // Pimage background;
+  // Pimage ;
+
+  // global variables
+
+  Ball ball = new Ball(100, 100, 5, 5, 20);
+
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
   }
 
   /** 
@@ -16,21 +22,54 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+
+    // need to get an image...
+
+    // background = loadImage("");
+
+    background(0);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
+	  background(0);
+    ball.update();
 	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+    
   }
   
-  // define other methods down here.
+  class Ball {
+    float xpos, ypos, xspeed, yspeed;
+    int size;
+    Ball (float x, float y, float velX, float velY, int ballSize){
+      xpos = x;
+      ypos = y;
+      xspeed = velX;
+      yspeed = velY;
+      size = ballSize;
+    }
+    public void update(){
+
+      if( (yspeed > 0) && ( Math.round(ypos) >= (height - size) ) ){
+        if ( ( (xpos >= (width - size) ) && xspeed > 0) || ( (xpos <= size) && xspeed < 0)){
+          xspeed *= -0.6;
+          yspeed *= 0.9;
+        }
+        if (ypos >= height - size || ypos <= size){
+          yspeed *= -0.8;
+          xspeed *= 0.8;
+        }
+      }
+      yspeed += 0.5;
+
+      xpos += xspeed;
+      ypos += yspeed;
+
+      fill (255);
+      circle(xpos, ypos, size);
+    }
+  }
+
 }
